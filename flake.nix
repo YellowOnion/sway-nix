6e4afe6392f9424e54238121c3476c3113cb978a
Daniel Hill <daniel@gluo.nz>
2024-04-24 22:45:08 +1200

    run nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -14,27 +14,27 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-typed-systems = {
+    typed-systems = {
       url = "github:YellowOnion/nix-typed-systems";
       flake = false;
     };
   };
 
-  outputs = { self
-            , nixpkgs
-            , typed-systems
-            , sway-src
-            , wlroots-src
-            , ...
-            }
-  :
+  outputs =
+    { self
+    , nixpkgs
+    , typed-systems
+    , sway-src
+    , wlroots-src
+    , ...
+    }:
     let
       inherit (import typed-systems) id genAttrsMapBy systems';
       systems = [ systems'.x86_64-linux systems'.aarch64-linux ];
 
       wlrVersion = "wlroots";
       overlay = self: super: {
-        sway-unwrapped = (super.sway-unwrapped.override {wlroots = self.wlroots;}).overrideAttrs (a: {
+        sway-unwrapped = (super.sway-unwrapped.override { wlroots = self.wlroots; }).overrideAttrs (a: {
           version = "${a.version}-deferred-cursor";
           src = sway-src;
         });
@@ -43,19 +43,24 @@ typed-systems = {
           src = wlroots-src;
         });
       };
-    in {
-      packages = (genAttrsMapBy id (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ overlay ];
-          };
-        in {
-          default = pkgs.sway;
-          sway = pkgs.sway;
-          sway-unwrapped = pkgs.sway-unwrapped;
-          wlroots   = pkgs.${wlrVersion};
-        }) systems id
+    in
+    {
+      packages = (genAttrsMapBy id
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ overlay ];
+            };
+          in
+          {
+            default = pkgs.sway;
+            sway = pkgs.sway;
+            sway-unwrapped = pkgs.sway-unwrapped;
+            wlroots = pkgs.${wlrVersion};
+          })
+        systems
+        id
       );
 
       overlays.default = overlay;
