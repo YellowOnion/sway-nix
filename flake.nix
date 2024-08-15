cab223cbc940d7a020e8097c6cac96f04e7cf94d
Daniel Hill <daniel@gluo.nz>
2024-08-16 01:31:08 +1200

    wlroots: v0.17.2 -> 0.17.4

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "sway-deferred-cursor";
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "nixpkgs/c3aa7b8938b17aebd2deecf7be0636000d62a2b9";
     sway-src = {
       url = "github:YellowOnion/sway/deferred-cursor-move";
       flake = false;
@@ -32,9 +32,10 @@
       inherit (import typed-systems) id genAttrsMapBy systems';
       systems = [ systems'.x86_64-linux systems'.aarch64-linux ];
 
-      wlrVersion = "wlroots";
+      wlrVersion = "wlroots_0_17";
       overlay = self: super: {
-        sway-unwrapped = (super.sway-unwrapped.override { wlroots = self.wlroots; }).overrideAttrs (a: {
+        sway-untouched = super.sway-unwrapped;
+        sway-unwrapped = (super.sway-unwrapped.override { wlroots = self."${wlrVersion}"; }).overrideAttrs (a: {
           version = "${a.version}-deferred-cursor";
           src = sway-src;
         });
@@ -56,6 +57,7 @@
           {
             default = pkgs.sway;
             sway = pkgs.sway;
+            sway-untouched = pkgs.sway-untouched;
             sway-unwrapped = pkgs.sway-unwrapped;
             wlroots = pkgs.${wlrVersion};
           })
